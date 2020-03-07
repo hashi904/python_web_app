e4f0fd0d9365f6aecd8d55edd674bbac7e8443c7
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-03-07 22:00:35 +0900

    Xpathを用いてxmlを読み込み
    設定ファイル(conf.xml)を外出しして、アプリの管理をしやすくした。
    Xpathを用いてconf.xmlのportノードからポート番号を取得し、サーバーを立てるようにした

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 import http.server
 import socketserver
+import xml.etree.ElementTree as ET
 
-PORT = 10891
+# xpathを使ってport番号を取得
+tree = ET.parse('conf.xml')
+root = tree.getroot()
+PORT = int(root.findall('.//server/port')[0].text)
+
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
